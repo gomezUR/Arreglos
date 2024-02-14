034638e88134ab10093b9cbc8b63426e36dfb40d
gomezUR <gomezrangeljuanesteban@gmail.com>
2024-02-14 14:47:54 -0500

    actividad en clase

---
+++ MATRICES/calculadora matrices .cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+    string calculadora [5][3];
+
+    calculadora[0][0]="1";
+    calculadora[0][1]="2";
+    calculadora[0][2]="3";
+    calculadora[1][0]="4";
+    calculadora[1][1]="5";
+    calculadora[1][2]="6";
+    calculadora[2][0]="7";
+    calculadora[2][1]="8";
+    calculadora[2][2]="9";
+    calculadora[3][0]="+";
+    calculadora[3][1]="0";
+    calculadora[3][2]="-";
+    calculadora[4][0]="*";
+    calculadora[4][1]="=";
+    calculadora[4][2]="√";
+
+    for (int i = 0; i < 5; i++);
+    for (int i = 0; j < 3; j++);
+
+}
